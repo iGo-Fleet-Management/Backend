b3eaf44264604ad6225f4225480761eee918d611
Hugo Carvalho <hugocarvmelo@gmail.com>
2025-04-30 20:22:49 -0300

    Alterando blacklist para 10min

--- Backend/src/config/blacklistCleanup.js
+++ Backend/src/config/blacklistCleanup.js
@@ -12,7 +12,7 @@ function startBlacklistCleanup() {
   // Executa imediatamente na inicialização
   clean().then(() => {
     // Depois da primeira execução, inicia o intervalo
-    setInterval(clean, 60 * 60 * 1000);
+    setInterval(clean, 10 * 60 * 1000);
   });
 }
 
